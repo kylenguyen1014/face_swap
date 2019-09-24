ffade6b7cf24b3004bbf173799ccac203679e368
Trung Nguyen <43191329+kylenguyen1014@users.noreply.github.com>
2019-09-24 19:20:43 -0400

    able to fetch detectAPI to get a template rectangle

--- src/component/Images.js
+++ src/component/Images.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
 import './Images.css';
-
+import axios from 'axios';
 
 function Images() {
     const [template, setTemplate] = useState();
     const [merging, setMerging] = useState();
+    const [result, setResult] = useState();
+    const APIkey = '1ltOS1IXZ1VTu_UyqY3S0HbV_DK3EJwA';
+    const APISecret = '2h4nPYp5Hs8qUAsfOM3H8TzQTKuoMZKZ';
+
 
     const templateImageChange = (e) => {
         let templateInput = e.target;
@@ -31,31 +35,65 @@ function Images() {
           };
           if (templateInput.files[0]){
             reader.readAsDataURL(templateInput.files[0]);
-          }    }
+          }    
+    }
+
+    const submitAPI = () => {
+        // axios.post('https://api-us.faceplusplus.com/facepp/v3/detect', {
+        //     api_key: APIkey,
+        //     api_secret: APISecret,
+        //     image_base64: template, 
+        // })
+        // .then((response) => {
+        //     console.log(response);
+        // }, (error) => {
+        //     console.log(error);
+        // })
+
+        fetch('http://localhost:8000/',{
+            method : 'post',
+            headers: {'Content-type': 'application/json'},
+            body : JSON.stringify({
+                template: template
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }
     return (
         <div className='Images mx-auto '>
             <div className="card" style={{width: '20rem', height : '25rem'}}>
+                <h5 className="card-title">Template Image</h5>
                 <div className='m-auto pt-3' style={{width: '12rem', height : '15rem'}}>
                     {(template) && <img src={template} className="card-img-top "  alt="template pic"/>}
                 </div>
                 <div className="card-body d-flex flex-column justify-content-end">
-                    <h5 className="card-title">Template Image</h5>
                     <div className="custom-file">
                         <input type="file" className="input-group-text w-100 bg-transparent border border-dark "   id="customFile1" accept="image/jpeg" onChange={templateImageChange}/>
                     </div>
                 </div>
             </div>
             <div className="card" style={{width: '20rem', height : '25rem'}}>
+                <h5 className="card-title">Merging Image</h5>
                 <div className='m-auto pt-3' style={{width: '12rem', height : '15rem'}}>
                     {(merging) && <img src={merging} className="card-img-top "  alt="template pic"/>}
                 </div>
                 <div className="card-body d-flex flex-column justify-content-end">
-                    <h5 className="card-title">Template Image</h5>
                     <div className="custom-file">
                         <input type="file" className="input-group-text w-100 bg-transparent border border-dark "   id="customFile1" accept="image/jpeg" onChange={mergingImageChange}/>
                     </div>
                 </div>
             </div>
+            <div className="card" style={{width: '20rem', height : '25rem'}}>
+                <h5 className="card-title">Result Image</h5>
+                <div className='m-auto pt-3' style={{width: '12rem', height : '15rem'}}>
+                    {(result) && <img src={result} className="card-img-top "  alt="template pic"/>}
+                </div>
+                <div className="card-body d-flex flex-column justify-content-end">
+                </div>
+            </div>
+            
+            <button onClick={submitAPI}>Submit</button>
         </div>
     )
 }
